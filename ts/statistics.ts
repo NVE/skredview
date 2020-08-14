8ff9c2366fade97d72cc95c09f0e8cfb0341def6
Aron Widforss <arwi@nve.no>
2020-08-14 17:00:18 +0200

    Add statistics table

---
+++ ts/statistics.ts
@@ -0,0 +1,55 @@
+import {getPrecision, OlObjects} from "./ol";
+import Feature from "ol/Feature";
+import {EXPOSITIONS, EXPOSITIONS_NO} from "./charts";
+
+function calculateStatistics(ol: OlObjects): void {
+    let div = document.getElementById("statistics");
+    let statAvalanchesAll = document.getElementById("stat-avalanches-all");
+    let statAvalanches24h = document.getElementById("stat-avalanches-24");
+    let statSizeMedian = document.getElementById("stat-size-median");
+    let statSizeMean = document.getElementById("stat-size-mean");
+    let statSizeMin = document.getElementById("stat-size-min");
+    let statSizeMax = document.getElementById("stat-size-max");
+    let statAspectMedian = document.getElementById("stat-aspect-median");
+
+    let events = [];
+    for (let dateString of Object.keys(ol.eventsByDate)) {
+        for (let id of Object.keys(ol.eventsByDate[dateString])) {
+            events.push(ol.eventsByDate[dateString][id]);
+        }
+    }
+    let len = events.length;
+    if (!len) return;
+
+    let areas = events.map((event: Feature) => parseInt(event.get("area"))).sort();
+    let aspects = [0, 0, 0, 0, 0, 0, 0, 0];
+    events.map((event: Feature) => {
+        let idx = EXPOSITIONS_NO.indexOf(event.get("eksposisjonUtlosningsomr"));
+        return idx >= 0 ? idx : null;
+    }).filter((idx) => idx !== null).forEach((idx) => {
+        aspects[idx] += 1;
+    });
+    let medianAspect = Math.max(...aspects) > 0 ? EXPOSITIONS[aspects.indexOf(Math.max(...aspects))] : "";
+
+    let median = len % 2 == 0 ? (areas[len / 2] + areas[len / 2 - 1]) / 2 : areas[Math.floor(len / 2)];
+    let mean = areas.reduce((x, y) => x + y) / (areas.length);
+    statAvalanchesAll.innerText = events.length.toString();
+    statAvalanches24h.innerText = events.filter((event: Feature) => getPrecision(event) < 24).length.toString();
+    statSizeMedian.innerText = median.toFixed(0);
+    statSizeMean.innerText = mean.toFixed(0);
+    statSizeMin.innerText = Math.min(...areas).toFixed(0);
+    statSizeMax.innerText = Math.max(...areas).toFixed(0);
+    statAspectMedian.innerText = medianAspect;
+}
+
+function clearStatistics(): void {
+    document.getElementById("stat-avalanches-all").innerText = "";
+    document.getElementById("stat-avalanches-24").innerText = "";
+    document.getElementById("stat-size-median").innerText = "";
+    document.getElementById("stat-size-mean").innerText = "";
+    document.getElementById("stat-size-min").innerText = "";
+    document.getElementById("stat-size-max").innerText = "";
+    document.getElementById("stat-aspect-median").innerText = "";
+}
+
+export {calculateStatistics, clearStatistics}
